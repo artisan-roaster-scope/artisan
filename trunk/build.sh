ea198108e0768c5970d4ce67dcef8e0983e4b70d
marko.luther@gmx.net <marko.luther@gmx.net@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2011-01-11 15:33:17 +0000

    updated setup for Mac OS X 10.5.x Leopard support

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+export MACOSX_DEPLOYMENT_TARGET=10.5
+export PYTHONPATH="/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages"
 rm -rf build dist
 python-all setup-mac.py py2app
