824c0783a6f7bb356f344dbbcd4f35f1cf77282d
marko.luther@gmx.net <marko.luther@gmx.net@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2011-01-11 20:17:28 +0000

    v0.3.0 release

---
+++ tags/0.3.0/setup-win.py
@@ -0,0 +1,54 @@
+"""
+This is a set up script for py2exe
+
+USAGE: python setup-win py2exe
+
+"""
+
+from distutils.core import setup
+import matplotlib as mpl
+import py2exe
+
+INCLUDES = [
+            "sip",
+            "serial",
+            ]
+
+EXCLUDES = ['_tkagg',
+            '_ps',
+            '_fltkagg',
+            'Tkinter',
+            'Tkconstants',
+            '_cairo',
+            '_gtk',
+            'gtkcairo',
+            'pydoc',
+            'sqlite3',
+            'bsddb',
+            'curses',
+            'tcl',
+            '_wxagg',
+            '_gtagg',
+            '_cocoaagg',
+            '_wx']
+
+setup(
+    name ="Artisan",
+    author = "YOU",
+    windows=[{"script" : "E:\\Artisan\\artisan\\trunk\\artisan.pyw"}],
+    data_files = mpl.get_py2exe_datafiles(),
+    zipfile = None,
+    options={"py2exe" :{
+                        "packages": ['matplotlib','pytz'],
+                        "compressed": True,
+                        "unbuffered": True,
+                        "optimize":1,
+                        "bundle_files": 1,
+                        "dll_excludes":[
+                            'tcl84.dll','tk84.dll','libgdk-win32-2.0-0.dll',
+                            'libgdk_pixbuf-2.0-0.dll','libgobject-2.0-0.dll'],
+                        "includes" : INCLUDES,
+                        "excludes" : EXCLUDES}
+             }
+    )
+
