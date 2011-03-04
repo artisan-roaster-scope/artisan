248aba01c220015db4bda5afd8acc036eae43a57
marko.luther@gmx.net <marko.luther@gmx.net@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2011-03-04 22:48:37 +0000

    initialized Swedish translation

---
+++ translations/artisan_sv.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="sv">
+<context>
+    <name>Menu</name>
+    <message>
+        <location filename="const/UIconst.py" line="25"/>
+        <source>File</source>
+        <translation>Arkiv</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="28"/>
+        <source>New</source>
+        <translation>Ny</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="29"/>
+        <source>Open...</source>
+        <translation>Öppna...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="30"/>
+        <source>Open Recent</source>
+        <translation>Öppna Senaste</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="31"/>
+        <source>Import</source>
+        <translation>Importera</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="32"/>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="33"/>
+        <source>Save As...</source>
+        <translation>Spara Som...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="34"/>
+        <source>Export...</source>
+        <translation>Exportera...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="35"/>
+        <source>Save Graph</source>
+        <translation>Spara Graf</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="36"/>
+        <source>HTML Report</source>
+        <translation>HTML-Rapport</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="37"/>
+        <source>Print...</source>
+        <translation>Skriv Ut...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="40"/>
+        <source>Roast</source>
+        <translation>Rosta</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="43"/>
+        <source>Properties...</source>
+        <translation>Egenskaper...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="44"/>
+        <source>Background...</source>
+        <translation>Bakrund</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="45"/>
+        <source>Cup Profile...</source>
+        <translation>Koppningsprofil...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="46"/>
+        <source>Temperature</source>
+        <translation>Temperatur</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="47"/>
+        <source>Calculator</source>
+        <translation>Beräkningar</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="50"/>
+        <source>Config</source>
+        <translation>Konfigurera</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="53"/>
+        <source>Device...</source>
+        <translation>Apparat...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="54"/>
+        <source>Serial Port...</source>
+        <translation>Serieport...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="55"/>
+        <source>Sampling Interval...</source>
+        <translation>Samplingsintervall...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="56"/>
+        <source>Colors...</source>
+        <translation>Färger...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="57"/>
+        <source>Phases...</source>
+        <translation>Faser...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="58"/>
+        <source>Events...</source>
+        <translation>Händelser...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="59"/>
+        <source>Statistics...</source>
+        <translation>Statistik...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="60"/>
+        <source>Axes...</source>
+        <translation>Axlar...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="61"/>
+        <source>Autosave...</source>
+        <translation>Spara Automatiskt...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="62"/>
+        <source>Extras...</source>
+        <translation>Extra...</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="65"/>
+        <source>Help</source>
+        <translation>Hjälp</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="70"/>
+        <source>Documentation</source>
+        <translation>Dokumentation</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="72"/>
+        <source>Errors</source>
+        <translation>Fel</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="71"/>
+        <source>Keyboard Shortcuts</source>
+        <translation>Tangentbordsgenvägar</translation>
+    </message>
+</context>
+</TS>
