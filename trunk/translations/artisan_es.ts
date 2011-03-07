5567b898a1aed4f03eaf6c609e40f3b71be31a8d
marko.luther@gmx.net <marko.luther@gmx.net@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2011-03-07 16:01:35 +0000

    fixed Spanish translation (thanks Bluequijote)

--- trunk/translations/artisan_es.ts
+++ trunk/translations/artisan_es.ts
@@ -15,7 +15,7 @@
     <message>
         <location filename="const/UIconst.py" line="30"/>
         <source>Open Recent</source>
-        <translation>Abrir Reciente</translation>
+        <translation>Abiertos Recientemente</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="31"/>
@@ -40,7 +40,7 @@
     <message>
         <location filename="const/UIconst.py" line="36"/>
         <source>HTML Report</source>
-        <translation>Informe HTML</translation>
+        <translation>Reporte en HTML</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="37"/>
@@ -50,7 +50,7 @@
     <message>
         <location filename="const/UIconst.py" line="40"/>
         <source>Roast</source>
-        <translation>Tostadura</translation>
+        <translation>Tostado</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="43"/>
@@ -65,7 +65,7 @@
     <message>
         <location filename="const/UIconst.py" line="45"/>
         <source>Cup Profile...</source>
-        <translation>Catacion...</translation>
+        <translation>Perfiles en Taza...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="46"/>
@@ -90,7 +90,7 @@
     <message>
         <location filename="const/UIconst.py" line="55"/>
         <source>Sampling Interval...</source>
-        <translation>Intervalo Tiempo...</translation>
+        <translation>Intervalo de Muestreo...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="56"/>
@@ -110,12 +110,12 @@
     <message>
         <location filename="const/UIconst.py" line="59"/>
         <source>Statistics...</source>
-        <translation>Estadisticas...</translation>
+        <translation>Estadísticas...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="61"/>
         <source>Autosave...</source>
-        <translation>Auto Grabar...</translation>
+        <translation>Auto-Grabar...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="62"/>
@@ -130,7 +130,7 @@
     <message>
         <location filename="const/UIconst.py" line="70"/>
         <source>Documentation</source>
-        <translation>Documentacion</translation>
+        <translation>Documentación</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="25"/>
@@ -140,12 +140,12 @@
     <message>
         <location filename="const/UIconst.py" line="35"/>
         <source>Save Graph</source>
-        <translation>Guardar Grafico</translation>
+        <translation>Guardar Gráfica</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="50"/>
         <source>Config</source>
-        <translation>Config</translation>
+        <translation>Configuración</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="60"/>
@@ -160,7 +160,7 @@
     <message>
         <location filename="const/UIconst.py" line="71"/>
         <source>Keyboard Shortcuts</source>
-        <translation>Atajo Teclado</translation>
+        <translation>Atajo de Teclado</translation>
     </message>
 </context>
 </TS>
--- trunk/translations/artisan_sv.ts
+++ trunk/translations/artisan_sv.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="sv">
+<!DOCTYPE TS><TS version="1.1" language="sv">
 <context>
     <name>Menu</name>
     <message>
