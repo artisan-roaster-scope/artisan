912eeacdf98be83698ad398c23e178f3979826d4
sdelgran@delgrande.org <sdelgran@delgrande.org@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2011-03-16 01:13:17 +0000

    Translated different types of strings

--- trunk/translations/artisan_fr.ts
+++ trunk/translations/artisan_fr.ts
@@ -1,171 +1,271 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>Form Caption</name>
+    <message>
+        <location filename="artisan.py" line="837"/>
+        <source>Profile unsaved</source>
+        <translation>Profil non sauvegardé</translation>
+    </message>
+</context>
+<context>
+    <name>Form Label</name>
+    <message>
+        <location filename="artisan.py" line="9125"/>
+        <source>Comm Port</source>
+        <translation>Port série</translation>
+    </message>
+    <message>
+        <location filename="artisan.py" line="9131"/>
+        <source>Baud Rate</source>
+        <translation>Bits par seconde</translation>
+    </message>
+</context>
+<context>
+    <name>Label</name>
+    <message>
+        <location filename="artisan.py" line="1126"/>
+        <source>BT</source>
+        <translation>TG</translation>
+    </message>
+</context>
 <context>
     <name>Menu</name>
     <message>
-        <location filename="const/UIconst.py" line="25"/>
-        <source>File</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="const/UIconst.py" line="28"/>
         <source>New</source>
-        <translation type="unfinished"></translation>
+        <translation>Nouveau</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="29"/>
         <source>Open...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="30"/>
         <source>Open Recent</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir Récent</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="31"/>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>Importer</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="32"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Enregistrer</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="33"/>
         <source>Save As...</source>
-        <translation type="unfinished"></translation>
+        <translation>Enregistrer Sous...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="34"/>
         <source>Export...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="const/UIconst.py" line="35"/>
-        <source>Save Graph</source>
-        <translation type="unfinished"></translation>
+        <translation>Exporter...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="36"/>
         <source>HTML Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Rapport HTML</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="37"/>
         <source>Print...</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimer...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="40"/>
         <source>Roast</source>
-        <translation type="unfinished"></translation>
+        <translation>Torrefaction</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="43"/>
         <source>Properties...</source>
-        <translation type="unfinished"></translation>
+        <translation>Propriétés...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="44"/>
         <source>Background...</source>
-        <translation type="unfinished"></translation>
+        <translation>Arrière Plan...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="45"/>
         <source>Cup Profile...</source>
-        <translation type="unfinished"></translation>
+        <translation>Dégustation...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="46"/>
         <source>Temperature</source>
-        <translation type="unfinished"></translation>
+        <translation>Température</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="47"/>
         <source>Calculator</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="const/UIconst.py" line="50"/>
-        <source>Config</source>
-        <translation type="unfinished"></translation>
+        <translation>Calculatrice</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="53"/>
         <source>Device...</source>
-        <translation type="unfinished"></translation>
+        <translation>Périphérique...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="54"/>
         <source>Serial Port...</source>
-        <translation type="unfinished"></translation>
+        <translation>Port Série...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="55"/>
         <source>Sampling Interval...</source>
-        <translation type="unfinished"></translation>
+        <translation>Intervalle d&apos;Échantillonnage...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="56"/>
         <source>Colors...</source>
-        <translation type="unfinished"></translation>
+        <translation>Couleurs...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="57"/>
         <source>Phases...</source>
-        <translation type="unfinished"></translation>
+        <translation>Phases...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="58"/>
         <source>Events...</source>
-        <translation type="unfinished"></translation>
+        <translation>Événements...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="59"/>
         <source>Statistics...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="const/UIconst.py" line="60"/>
-        <source>Axes...</source>
-        <translation type="unfinished"></translation>
+        <translation>Statistiques...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="61"/>
         <source>Autosave...</source>
-        <translation type="unfinished"></translation>
+        <translation>Enregistrement Automatique...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="62"/>
         <source>Extras...</source>
-        <translation type="unfinished"></translation>
+        <translation>Suppléments...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="65"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Aide</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="70"/>
         <source>Documentation</source>
-        <translation type="unfinished"></translation>
+        <translation>Documentation</translation>
     </message>
     <message>
-        <location filename="const/UIconst.py" line="71"/>
-        <source>Keyboard Shortcuts</source>
-        <translation type="unfinished"></translation>
+        <location filename="const/UIconst.py" line="25"/>
+        <source>File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="35"/>
+        <source>Save Graph</source>
+        <translation>Enregistrer le Graphique</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="50"/>
+        <source>Config</source>
+        <translation>Configuration</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="60"/>
+        <source>Axes...</source>
+        <translation>Axes...</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="72"/>
         <source>Errors</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreurs</translation>
+    </message>
+    <message>
+        <location filename="const/UIconst.py" line="71"/>
+        <source>Keyboard Shortcuts</source>
+        <translation>Raccourcis Clavier</translation>
     </message>
     <message>
         <location filename="const/UIconst.py" line="73"/>
         <source>Messages</source>
+        <translation>Messages</translation>
+    </message>
+</context>
+<context>
+    <name>Message Area</name>
+    <message>
+        <location filename="artisan.py" line="545"/>
+        <source>HUD OFF</source>
+        <translation>VTH désactivée</translation>
+    </message>
+    <message>
+        <location filename="artisan.py" line="557"/>
+        <source>HUD ON</source>
+        <translation>VTH en activée</translation>
+    </message>
+    <message>
+        <location filename="artisan.py" line="559"/>
+        <source>Need some data for HUD to work</source>
+        <translation>Des données sont nécessaires pour activer la VTH. </translation>
+    </message>
+    <message>
+        <location filename="artisan.py" line="842"/>
+        <source>Reset has been cancelled</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="artisan.py" line="873"/>
+        <source>Scope has been reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Message Box</name>
+    <message>
+        <location filename="artisan.py" line="836"/>
+        <source>Do you want to save the profile?</source>
+        <translation>Voulez-vous sauvegarder le profil ?</translation>
+    </message>
+</context>
+<context>
+    <name>Scope Button</name>
+    <message>
+        <location filename="artisan.py" line="2709"/>
+        <source>ON</source>
+        <translation>MARCHE</translation>
+    </message>
+</context>
+<context>
+    <name>Scope Label</name>
+    <message>
+        <location filename="artisan.py" line="1126"/>
+        <source>ET</source>
+        <translation>TE</translation>
+    </message>
+    <message>
+        <location filename="artisan.py" line="1131"/>
+        <source>DeltaET</source>
+        <translation>DeltaTE</translation>
+    </message>
+    <message>
+        <location filename="artisan.py" line="1135"/>
+        <source>DeltaBT</source>
+        <translation>DeltaTG</translation>
+    </message>
+</context>
+<context>
+    <name>Tooltip</name>
+    <message>
+        <location filename="artisan.py" line="2714"/>
+        <source>Starts recording</source>
+        <translation>Démarre l&apos;enregistrement</translation>
     </message>
 </context>
 </TS>
