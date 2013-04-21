352b3396b0b0cf63133785caeb35fdfe816bfa65
luther.marko@googlemail.com <luther.marko@googlemail.com@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2013-04-21 18:16:20 +0000

    - python, numpy, scipy, matplotlib, py2app libs updated - fixed load/save file path handling to support foreign characters - fixed BT/ET reading producing lines crossing the whole graph - improved handling of the "dirty flag" that prevents the loose of unsaved changes - adds drop filter (experimental) - adds roast color and some roast property flags - improved serial port scanning - reorganized Extras dialog - extended Modbus read register support (function 4 and floats added) - default directory on Mac moved from ~/Library to ~/Documents - bug fixes

--- trunk/const/UIconst.py
+++ trunk/const/UIconst.py
@@ -106,6 +106,7 @@ if platf != 'Darwin':
 #note that the "About" menu item is recognized only if it is named "About", but automatically translated by the Qt standard tranlators
 HELP_MENU_ABOUT = "About" #QApplication.translate("Menu", "About", None, QApplication.UnicodeUTF8)
 HELP_MENU_DOCUMENTATION = QApplication.translate("Menu", "Documentation", None, QApplication.UnicodeUTF8)
+HELP_MENU_BLOG = QApplication.translate("Menu", "Blog", None, QApplication.UnicodeUTF8)
 HELP_MENU_KEYBOARDSHORTCUTS = QApplication.translate("Menu", "Keyboard Shortcuts", None, QApplication.UnicodeUTF8)
 HELP_MENU_ERRORS = QApplication.translate("Menu", "Errors", None, QApplication.UnicodeUTF8)
 HELP_MENU_MESSAGES = QApplication.translate("Menu", "Messages", None, QApplication.UnicodeUTF8)
