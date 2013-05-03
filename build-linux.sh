613b64e30cb9a5720a48948fc47dc266046420db
marko.luther@gmx.net <marko.luther@gmx.net@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2013-05-03 13:01:59 +0000

    adds linux .rpm .dep package build script

--- build-linux.sh
+++ build-linux.sh
@@ -3,17 +3,17 @@
 
 # build CentOS .rpm
 
-rm -rf debian/share/artisan
+rm -rf debian/usr/share/artisan
 tar -xf dist-centos.tar -C debian/usr/share
 mv debian/usr/share/dist debian/usr/share/artisan
-find debian -name .svn -exec rm -rf {} \;
+find debian -name .svn -exec rm -rf {} \; | /dev/null
 dpkg --build debian artisan-0.6.0_i386.deb
-alien -r debian artisan-0.6.0_i386.deb
+alien -r artisan-0.6.0_i386.deb
 
 # build Ubuntu .deb
 
-rm -rf debian/share/artisan
+rm -rf debian/usr/share/artisan
 tar -xf dist-ubuntu.tar -C debian/usr/share
 mv debian/usr/share/dist debian/usr/share/artisan
-find debian -name .svn -exec rm -rf {} \;
+find debian -name .svn -exec rm -rf {} \; | /dev/null
 dpkg --build debian artisan-0.6.0_i386.deb
