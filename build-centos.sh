4f86a73b9b947cac1fad5d96a57cbd1d2b26e00b
marko.luther@gmx.net <marko.luther@gmx.net@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2013-05-03 12:15:03 +0000

    updated build setup

---
+++ build-centos.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# translations
+pylupdate4 artisan.pro
+lrelease -verbose artisan.pro
+
+# distribution
+rm -rf dist
+bbfreeze artisan.py
+cp -R /usr/local/lib/python2.7/dist-packages/matplotlib/mpl-data/ dist
+cp -R Wheels dist
+cp README.txt dist
+cp LICENSE.txt dist
+mkdir dist/translations
+cp translations/*.qm dist/translations
+tar -cf dist-centos.tar dist
