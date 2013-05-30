0d7ae858f434ca8017bd8d1a5259a82bbf2e5178
luther.marko@googlemail.com <luther.marko@googlemail.com@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2013-05-30 19:42:07 +0000

    improved linux build setup to make lintian happy

--- build-centos64.sh
+++ build-centos64.sh
@@ -7,12 +7,12 @@ lrelease -verbose artisan.pro
 # distribution
 rm -rf dist
 bbfreeze artisan.py
-cp -L /usr/lib64/libz.so dist
-cp -L /usr/lib64/libxml2.so.2 dist
-cp -L /usr/lib64/libicudata.so.42 dist
-cp -L /usr/lib64/libicuuc.so.42 dist
-cp -L /usr/lib64/libicui18n.so.42 dist
-cp -L /usr/lib64/gnome-keyring/gnome-keyring-pkcs11.so dist
+#cp -L /usr/lib64/libz.so dist
+#cp -L /usr/lib64/libxml2.so.2 dist
+#cp -L /usr/lib64/libicudata.so.42 dist
+#cp -L /usr/lib64/libicuuc.so.42 dist
+#cp -L /usr/lib64/libicui18n.so.42 dist
+#cp -L /usr/lib64/gnome-keyring/gnome-keyring-pkcs11.so dist
 cp -R /usr/local/lib/python2.7/site-packages/matplotlib/mpl-data/ dist
 cp artisan-alog.xml dist
 cp artisan-alrm.xml dist
@@ -26,10 +26,15 @@ mkdir dist/Resources/qt_plugins
 mkdir dist/Resources/qt_plugins/imageformats
 mkdir dist/Resources/qt_plugins/iconengines
 cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqsvg.so dist/Resources/qt_plugins/imageformats
+patchelf --set-rpath ${ORIGIN}:${ORIGIN}/../lib dist/Resources/qt_plugins/imageformats/libqsvg.so
 cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqgif.so dist/Resources/qt_plugins/imageformats
+patchelf --set-rpath ${ORIGIN}:${ORIGIN}/../lib dist/Resources/qt_plugins/imageformats/libqgif.so
 cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqjpeg.so dist/Resources/qt_plugins/imageformats
-cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqtiff.so dist/Resources/qt_plugins/imageformats
+patchelf --set-rpath ${ORIGIN}:${ORIGIN}/../lib dist/Resources/qt_plugins/imageformats/libqjpeg.so
+#cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqtiff.so dist/Resources/qt_plugins/imageformats
+#patchelf --set-rpath ${ORIGIN}:${ORIGIN}/../lib dist/Resources/qt_plugins/imageformats/libqtiff.so
 cp /usr/local/Trolltech/Qt-4.8.4/plugins/iconengines/libqsvgicon.so dist/Resources/qt_plugins/iconengines
+patchelf --set-rpath ${ORIGIN}:${ORIGIN}/../lib dist/Resources/qt_plugins/iconengines/libqsvgicon.so
 cp qt.conf dist
 mkdir dist/translations
 cp translations/*.qm dist/translations
