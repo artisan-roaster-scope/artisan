d9a5be2784a1b91cfbdc71bb8a73b7a866e6f9ea
luther.marko@googlemail.com <luther.marko@googlemail.com@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2013-05-30 09:17:18 +0000

    adds centOS 64bit build script

---
+++ trunk/build-centos64.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# translations
+pylupdate4 artisan.pro
+lrelease -verbose artisan.pro
+
+# distribution
+rm -rf dist
+bbfreeze artisan.py
+cp -L /usr/lib64/libz.so dist
+cp -L /usr/lib64/libxml2.so.2 dist
+cp -L /usr/lib64/libicudata.so.42 dist
+cp -L /usr/lib64/libicuuc.so.42 dist
+cp -L /usr/lib64/libicui18n.so.42 dist
+cp -L /usr/lib64/gnome-keyring/gnome-keyring-pkcs11.so dist
+cp -R /usr/local/lib/python2.7/site-packages/matplotlib/mpl-data/ dist
+cp artisan-alog.xml dist
+cp artisan-alrm.xml dist
+cp artisan-apal.xml dist
+cp -R icons dist
+cp -R Wheels dist
+cp README.txt dist
+cp LICENSE.txt dist
+mkdir dist/Resources
+mkdir dist/Resources/qt_plugins
+mkdir dist/Resources/qt_plugins/imageformats
+mkdir dist/Resources/qt_plugins/iconengines
+cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqsvg.so dist/Resources/qt_plugins/imageformats
+cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqgif.so dist/Resources/qt_plugins/imageformats
+cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqjpeg.so dist/Resources/qt_plugins/imageformats
+cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqtiff.so dist/Resources/qt_plugins/imageformats
+cp /usr/local/Trolltech/Qt-4.8.4/plugins/iconengines/libqsvgicon.so dist/Resources/qt_plugins/iconengines
+cp qt.conf dist
+mkdir dist/translations
+cp translations/*.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_de.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_es.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_fr.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_sv.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_zh_CN.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_zh_TW.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_ko.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_pt.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_ru.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_ar.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_ja.qm dist/translations
+tar -cf dist-centos64.tar dist
