f2239992bab6faf8090977394531b3db292b7272
luther.marko@googlemail.com <luther.marko@googlemail.com@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2013-05-30 19:42:07 +0000

    improved linux build setup to make lintian happy

--- trunk/build-linux.sh
+++ trunk/build-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 VERSION=$(python -c 'import artisanlib; print(artisanlib.__version__)')
-NAME=artisan-${VERSION}
+NAME=artisan-linux-${VERSION}
 
 # fix debian/DEBIAN/control _VERSION_
 sed -i "s/_VERSION_/${VERSION}/g" debian/DEBIAN/control
@@ -13,8 +13,9 @@ tar -xf dist-centos.tar -C debian/usr/share
 mv debian/usr/share/dist debian/usr/share/artisan
 find debian -name .svn -exec rm -rf {} \; > /dev/null 2>&1
 chown -R root:root debian
+chmod -R g-w debian
 rm ${NAME}_i386.deb
-dpkg --build debian ${NAME}_i386.deb
+fakeroot dpkg --build debian ${NAME}_i386.deb
 alien -r ${NAME}_i386.deb
 mv ${NAME}-2.i386.rpm ${NAME}_i386.rpm
 
@@ -25,8 +26,9 @@ tar -xf dist-ubuntu.tar -C debian/usr/share
 mv debian/usr/share/dist debian/usr/share/artisan
 find debian -name .svn -exec rm -rf {} \; > /dev/null 2>&1
 chown -R root:root debian
+chmod -R g-w debian
 rm ${NAME}_i386-glibc2.4.deb
-dpkg --build debian ${NAME}_i386-glibc2.4.deb
+fakeroot dpkg --build debian ${NAME}_i386-glibc2.4.deb
 
 # build CentOS amd64 .rpm
 
@@ -37,8 +39,9 @@ tar -xf dist-centos64.tar -C debian/usr/share
 mv debian/usr/share/dist debian/usr/share/artisan
 find debian -name .svn -exec rm -rf {} \; > /dev/null 2>&1
 chown -R root:root debian
+chmod -R g-w debian
 rm ${NAME}_amd64.deb
-dpkg --build debian ${NAME}_amd64.deb
+fakeroot dpkg --build debian ${NAME}_amd64.deb
 alien -r ${NAME}_amd64.deb
 mv ${NAME}-2.amd64.rpm ${NAME}_amd64.rpm
 
