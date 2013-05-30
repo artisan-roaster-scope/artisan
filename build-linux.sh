f0e31d1f792ee3a000236b6459b7ff9a8f089a42
luther.marko@googlemail.com <luther.marko@googlemail.com@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2013-05-30 09:27:30 +0000

    64bit build instructions

--- build-linux.sh
+++ build-linux.sh
@@ -6,7 +6,7 @@ NAME=artisan-${VERSION}
 # fix debian/DEBIAN/control _VERSION_
 sed -i "s/_VERSION_/${VERSION}/g" debian/DEBIAN/control
 
-# build CentOS .rpm
+# build CentOS i386 .rpm
 
 rm -rf debian/usr/share/artisan
 tar -xf dist-centos.tar -C debian/usr/share
@@ -27,6 +27,23 @@ find debian -name .svn -exec rm -rf {} \; > /dev/null 2>&1
 chown -R root:root debian
 rm ${NAME}_i386-glibc2.4.deb
 dpkg --build debian ${NAME}_i386-glibc2.4.deb
+
+# build CentOS amd64 .rpm
+
+sed -i "s/i386/amd64/g" debian/DEBIAN/control
+
+rm -rf debian/usr/share/artisan
+tar -xf dist-centos64.tar -C debian/usr/share
+mv debian/usr/share/dist debian/usr/share/artisan
+find debian -name .svn -exec rm -rf {} \; > /dev/null 2>&1
+chown -R root:root debian
+rm ${NAME}_amd64.deb
+dpkg --build debian ${NAME}_amd64.deb
+alien -r ${NAME}_amd64.deb
+mv ${NAME}-2.amd64.rpm ${NAME}_amd64.rpm
+
+# fix debian/DEBIAN/control architecture
+sed -i "s/amd64/i386/g" debian/DEBIAN/control
 
 # fix debian/DEBIAN/control _VERSION_
 sed -i "s/${VERSION}/_VERSION_/g" debian/DEBIAN/control
