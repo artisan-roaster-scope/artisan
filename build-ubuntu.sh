9f871fd76b79e799671a3751454ef304f678d6de
luther.marko@googlemail.com <luther.marko@googlemail.com@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2013-11-18 21:57:24 +0000

    . updates linux build scripts . various smaller fixes

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -31,29 +31,29 @@ mkdir dist/Resources
 mkdir dist/Resources/qt_plugins
 mkdir dist/Resources/qt_plugins/imageformats
 mkdir dist/Resources/qt_plugins/iconengines
-cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqsvg.so dist/Resources/qt_plugins/imageformats
+cp /usr/local/Trolltech/Qt-4.8.5/plugins/imageformats/libqsvg.so dist/Resources/qt_plugins/imageformats
 patchelf --set-rpath '${ORIGIN}/../../..:${ORIGIN}/../../../../lib' dist/Resources/qt_plugins/imageformats/libqsvg.so
-cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqgif.so dist/Resources/qt_plugins/imageformats
+cp /usr/local/Trolltech/Qt-4.8.5/plugins/imageformats/libqgif.so dist/Resources/qt_plugins/imageformats
 patchelf --set-rpath '${ORIGIN}/../../..:${ORIGIN}/../../../../lib' dist/Resources/qt_plugins/imageformats/libqgif.so
-#cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqjpeg.so dist/Resources/qt_plugins/imageformats
+#cp /usr/local/Trolltech/Qt-4.8.5/plugins/imageformats/libqjpeg.so dist/Resources/qt_plugins/imageformats
 #patchelf --set-rpath '/../../..${ORIGIN}:${ORIGIN}/../../../../lib' dist/Resources/qt_plugins/imageformats/libqjpeg.so
-#cp /usr/local/Trolltech/Qt-4.8.4/plugins/imageformats/libqtiff.so dist/Resources/qt_plugins/imageformats
+#cp /usr/local/Trolltech/Qt-4.8.5/plugins/imageformats/libqtiff.so dist/Resources/qt_plugins/imageformats
 #patchelf --set-rpath '/../../..${ORIGIN}:${ORIGIN}/../../../../lib' dist/Resources/qt_plugins/imageformats/libqtiff.so
-cp /usr/local/Trolltech/Qt-4.8.4/plugins/iconengines/libqsvgicon.so dist/Resources/qt_plugins/iconengines
+cp /usr/local/Trolltech/Qt-4.8.5/plugins/iconengines/libqsvgicon.so dist/Resources/qt_plugins/iconengines
 patchelf --set-rpath '/../../..${ORIGIN}:${ORIGIN}/../../../../lib' dist/Resources/qt_plugins/iconengines/libqsvgicon.so
 cp qt.conf dist
 mkdir dist/translations
 cp translations/*.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_de.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_es.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_fr.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_sv.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_zh_CN.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_zh_TW.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_ko.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_pt.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_ru.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_ar.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_ja.qm dist/translations
-cp /usr/local/Trolltech/Qt-4.8.4/translations/qt_hu.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_de.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_es.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_fr.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_sv.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_zh_CN.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_zh_TW.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_ko.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_pt.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_ru.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_ar.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_ja.qm dist/translations
+cp /usr/local/Trolltech/Qt-4.8.5/translations/qt_hu.qm dist/translations
 tar -cf dist-ubuntu.tar dist
