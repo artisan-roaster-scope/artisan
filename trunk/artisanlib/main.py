0a3049a7807113bfa92d039b90e7b91023500737
luther.marko@googlemail.com <luther.marko@googlemail.com@22e2851c-de03-7ad9-de36-63c5d42d1a46>
2013-11-22 06:05:07 +0000

    typo

--- trunk/artisanlib/main.py
+++ trunk/artisanlib/main.py
@@ -7540,7 +7540,7 @@ class ApplicationWindow(QMainWindow):
         
     def getResourcePath(self):
         res = ""
-        if platf in ['Darwin',"Linux']:
+        if platf in ['Darwin','Linux']:
             if self.appFrozen():
                 res = QApplication.applicationDirPath() + "/../Resources/"
             else:
