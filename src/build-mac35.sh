ead54c0b7151e4bd25cdd23952421e7db7646f35
Rui Paulo <rpaulo@me.com>
2018-03-18 16:08:46 -0700

    Travis: fix glob

--- src/build-mac35.sh
+++ src/build-mac35.sh
@@ -7,7 +7,7 @@ if [ ! -z $TRAVIS ]; then
     export PYTHON=/usr/local
     export PYTHONPATH=$PYTHON/lib/python3.6/site-packages
     export PYTHON_V=3.6
-    export QT_PATH=/usr/local/Cellar/qt/*
+    export QT_PATH=`echo /usr/local/Cellar/qt/*`
     export MACOSX_DEPLOYMENT_TARGET=10.12
 else
     export PYTHON=/Library/Frameworks/Python.framework/Versions/3.5
