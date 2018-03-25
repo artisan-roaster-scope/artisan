e3f66cbf9fef40458e0b8725a9aab3414c50586e
Rui Paulo <rpaulo@me.com>
2018-03-24 23:20:15 -0700

    Different debugging test

--- src/build-win3-pi.bat
+++ src/build-win3-pi.bat
@@ -1,6 +1,6 @@
 @echo off
 
-pyinstaller -d -c --noconfirm artisan-win.spec
+pyinstaller --noconfirm artisan-win.spec
 
 rem #
 rem # Don't make assumptions as to where the 'makensis.exe' is - look in the obvious places
