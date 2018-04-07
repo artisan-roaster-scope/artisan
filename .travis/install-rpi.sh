7f7083fd202a18aa45b5b46b8cadff58f47ea166
Rui Paulo <rpaulo@me.com>
2018-04-06 22:50:03 -0700

    Build less of qemu

--- .travis/install-rpi.sh
+++ .travis/install-rpi.sh
@@ -5,9 +5,9 @@ sudo apt-get update
 sudo apt-get install p7zip-full libglib2.0-dev zlib1g-dev
 
 curl -L -O https://download.qemu.org/qemu-2.11.1.tar.xz
-tar xvJf qemu-2.11.1.tar.xz
+tar xJf qemu-2.11.1.tar.xz
 cd qemu-2.11.1
-./configure
+./configure --target-list=arm-softmmu
 make
 sudo make install
 cd ..
