ee5545736921b35fa2d6038554d4d6cd59146034
Rui Paulo <rpaulo@me.com>
2018-04-06 22:42:09 -0700

    Need to use the latest qemu.

--- .travis/install-rpi.sh
+++ .travis/install-rpi.sh
@@ -2,4 +2,12 @@
 
 set -ex
 sudo apt-get update
-sudo apt-get install qemu-system-arm p7zip-full
+sudo apt-get install p7zip-full libglib2.0-dev zlib1g-dev
+
+curl -L -O https://download.qemu.org/qemu-2.11.1.tar.xz
+tar xvJf qemu-2.11.1.tar.xz
+cd qemu-2.11.1
+./configure
+make
+sudo make install
+cd ..
