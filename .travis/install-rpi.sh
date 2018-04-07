05848744388e6b3b0779eaa1869a61e610f6ddb9
Rui Paulo <rpaulo@me.com>
2018-04-07 10:23:35 -0700

    RPI: mount root read-only

--- .travis/install-rpi.sh
+++ .travis/install-rpi.sh
@@ -7,7 +7,7 @@ sudo apt-get install p7zip-full libglib2.0-dev zlib1g-dev
 curl -L -O https://download.qemu.org/qemu-2.11.1.tar.xz
 tar xJf qemu-2.11.1.tar.xz
 cd qemu-2.11.1
-./configure # --target-list=arm-softmmu
+./configure --target-list=arm-softmmu
 make
 sudo make install
 cd ..
