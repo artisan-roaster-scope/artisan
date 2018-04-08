845fd4d10e7aebf49a6c77c90bd8723bc3a84f24
Rui Paulo <rpaulo@me.com>
2018-04-07 21:41:19 -0700

    Use cached libsnap/libphidget22

--- .travis/install-rpi.sh
+++ .travis/install-rpi.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 set -ex
-sudo apt-get install p7zip-full libglib2.0-dev zlib1g-dev qemu-system-arm
+sudo apt-get install p7zip-full libglib2.0-dev zlib1g-dev
 
 # Use updated qemu
-#curl -L -O https://download.qemu.org/qemu-2.11.1.tar.xz
-#tar xJf qemu-2.11.1.tar.xz
-#cd qemu-2.11.1
-#./configure --target-list=arm-softmmu
-#make -j4
-#sudo make install
-#cd ..
+curl -L -O https://download.qemu.org/qemu-2.11.1.tar.xz
+tar xJf qemu-2.11.1.tar.xz
+cd qemu-2.11.1
+./configure --target-list=arm-softmmu
+make -j4
+sudo make install
+cd ..
 
 # Use updated sfdisk
 curl -L -O https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.32/util-linux-2.32.tar.gz
--- src/build-rpi2-qemu.sh
+++ src/build-rpi2-qemu.sh
@@ -85,10 +85,10 @@ elif [ -f artisan.py ]; then
     sudo cp -R ../../artisan/LICENSE $mountpoint/home/pi/artisan
 fi
 cd $mountpoint/home/pi
-sudo curl -L -O https://astuteinternet.dl.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z
-sudo 7z x -bd snap7-full-1.4.2.7z >/dev/null
-sudo curl -L -O https://www.phidgets.com/downloads/phidget22/libraries/linux/libphidget22.tar.gz
-sudo tar -xzf libphidget22.tar.gz
+sudo curl -L -O https://dl.bintray.com/artisan/artisan-cache/libsnap.tar.gz
+sudo tar -C $mountpoint/usr/lib -xzf libsnap.tar.gz
+sudo curl -L -O https://dl.bintray.com/artisan/artisan-cache/libphidget22.tar.gz
+sudo tar -C $mountpoint/usr/lib -xzf libphidget22.tar.gz
 sudo curl -L -O https://www.phidgets.com/downloads/phidget22/libraries/any/Phidget22Python.zip
 sudo unzip -q Phidget22Python.zip
 sudo curl -L -O https://dl.bintray.com/artisan/artisan-cache/pip-cache.tar.gz   
