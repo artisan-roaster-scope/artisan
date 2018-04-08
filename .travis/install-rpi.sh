69517347cdd4e1b494f332d8d1aedeaa82dc0bc4
Rui Paulo <rpaulo@me.com>
2018-04-07 22:11:33 -0700

    Use prebuilt qemu img.

--- .travis/install-rpi.sh
+++ .travis/install-rpi.sh
@@ -4,13 +4,16 @@ set -ex
 sudo apt-get install p7zip-full libglib2.0-dev zlib1g-dev
 
 # Use updated qemu
-curl -L -O https://download.qemu.org/qemu-2.11.1.tar.xz
-tar xJf qemu-2.11.1.tar.xz
-cd qemu-2.11.1
-./configure --target-list=arm-softmmu
-make -j4
-sudo make install
-cd ..
+curl -L -O https://dl.bintray.com/artisan/artisan-cache/qemu.tar.gz
+tar xvf qemu.tar.gz
+sudo mv qemu-img qemu-system-arm /usr/bin
+#curl -L -O https://download.qemu.org/qemu-2.11.1.tar.xz
+#tar xJf qemu-2.11.1.tar.xz
+#cd qemu-2.11.1
+#./configure --target-list=arm-softmmu
+#make -j4
+#sudo make install
+#cd ..
 
 # Use updated sfdisk
 curl -L -O https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.32/util-linux-2.32.tar.gz
