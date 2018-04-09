b5123c4713be159270a10322e5b39755d2cf1c69
Rui Paulo <rpaulo@me.com>
2018-04-08 18:56:36 -0700

    Run pip3 in a loop.

--- src/build-rpi2-qemu.sh
+++ src/build-rpi2-qemu.sh
@@ -27,7 +27,12 @@ ssh_control()
     cat <<EOF > script
     sudo apt install -y python3-pip python3-pyqt5 libusb-1.0 \
 	    libblas-dev liblapack-dev libatlas-base-dev gfortran
-    pip3 install -r artisan/src/requirements.txt
+    while :; do
+        pip3 install -r artisan/src/requirements.txt
+	if [ $? -eq 0 ]; then
+	   break
+	fi
+    done       	
     (cd Phidget22Python && sudo python3 setup.py install)
     cd artisan/src
     ./build-centos-pi.sh
