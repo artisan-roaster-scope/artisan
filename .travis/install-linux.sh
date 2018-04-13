0ed2a209052cd8289a60049e9fefc2d712ddefda
Rui Paulo <rpaulo@me.com>
2018-04-12 18:09:01 -0700

    Separate pymodbus installation into its own script

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -20,3 +20,4 @@ curl -L -O https://kent.dl.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.
 7z x -bd snap7-full-1.4.2.7z
 (cd snap7-full-1.4.2/build/unix && make -f x86_64_linux.mk all && sudo make -f x86_64_linux.mk install)
 
+.travis/install-pymodbus.sh
--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -16,5 +16,4 @@ hdiutil attach Phidget22.dmg
 sudo installer -pkg /Volumes/Phidgets22/Phidgets.pkg -target /
 sudo rm -rf /usr/local/lib/python3.6/site-packages/matplotlib/mpl-data/sample_data
 
-git clone -b python3 https://github.com/riptideio/pymodbus.git
-(cd pymodbus && patch -p0 < ../src/patches/pymodbus.patch && python3 setup.py install)
+.travis/install-pymodbus.sh
