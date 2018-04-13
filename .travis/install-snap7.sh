b1cec5ab18649acffd25d90b28ca6f3238fdf32d
Rui Paulo <rpaulo@me.com>
2018-04-12 18:21:58 -0700

    Fix snap7 script.

--- .travis/install-snap7.sh
+++ .travis/install-snap7.sh
@@ -7,11 +7,13 @@ version=1.4.2
 if [ "$ARTISAN_OS" = "linux" ]; then
     libinstall=/usr/lib
     mkfile=x86_64_linux
+    os="unix"
 elif [ "$ARTISAN_OS" = "osx" ]; then
     libinstall=/usr/local/lib
     mkfile=x86_64_osx
+    os="osx"
 fi
 
 curl -L -O https://kent.dl.sourceforge.net/project/snap7/${version}/snap7-full-${version}.7z
 7z x -bd snap7-full-${version}.7z
-(cd snap7-full-${version}/build/${ARTISAN_OS} && make -f ${mkfile}.mk all && sudo make -f ${mkfile}.mk LibInstall=${libinstall} install)
+(cd snap7-full-${version}/build/${os} && make -f ${mkfile}.mk all && sudo make -f ${mkfile}.mk LibInstall=${libinstall} install)
