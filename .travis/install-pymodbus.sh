1c51b8e27d930df468990da6b64e5fb212fd5f89
Rui Paulo <rpaulo@me.com>
2018-04-13 17:38:43 -0700

    Fix pymodbus script on RPI.

--- .travis/install-pymodbus.sh
+++ .travis/install-pymodbus.sh
@@ -2,5 +2,11 @@
 
 set -ex
 
+if [ "$ARTISAN_OS" = "rpi" ]; then
+    SUDO="sudo"
+else
+    SUDO=""
+fi
+
 git clone -b python3 https://github.com/riptideio/pymodbus.git
-(cd pymodbus && patch -p0 < ../src/patches/pymodbus.patch && python3 setup.py install)
+(cd pymodbus && patch -p0 < ../src/patches/pymodbus.patch && $SUDO python3 setup.py install)
