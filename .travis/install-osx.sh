8434c7cefca640d6170f092aa056f354fd8119e9
Rui Paulo <rpaulo@me.com>
2018-04-14 14:28:57 -0700

    OSX: built qt 5.10.0 from source.

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -3,6 +3,14 @@
 set -ex
 brew upgrade python
 brew install p7zip
+curl -L -O https://download.qt.io/archive/qt/5.10/5.10.0/single/qt-everywhere-src-5.10.0.tar.xz
+tar xzf qt-everywhere-src-5.10.0.tar.xz
+cd qt-everywhere-src-5.10.0
+./configure
+make -j4
+make install
+cd ..
+
 pip3 install -r src/requirements.txt
 pip3 install -r src/requirements-${TRAVIS_OS_NAME}.txt
 sudo rm -rf /usr/local/lib/python3.6/site-packages/matplotlib/mpl-data/sample_data
