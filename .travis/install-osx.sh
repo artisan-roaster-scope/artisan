d7e29e96274cc2b9b9b925a0dccc1bf5a038fe1f
Rui Paulo <rpaulo@me.com>
2018-07-04 19:37:27 -0700

    Update to Python 3.7.

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -13,7 +13,7 @@ brew install p7zip
 
 pip3 install -r src/requirements.txt
 pip3 install -r src/requirements-${TRAVIS_OS_NAME}.txt
-sudo rm -rf /usr/local/lib/python3.6/site-packages/matplotlib/mpl-data/sample_data
+sudo rm -rf /usr/local/lib/python3.7/site-packages/matplotlib/mpl-data/sample_data
 
 .travis/install-phidgets.sh
 .travis/install-snap7.sh
