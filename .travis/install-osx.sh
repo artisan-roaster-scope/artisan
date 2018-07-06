7b6bf6ecc42a065d34ae0dc4451758922bf39ef9
MAKOMO <marko.luther@gmx.net>
2018-07-06 17:34:31 +0200

    try upgrade instead of install

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -4,7 +4,7 @@ set -ex
 #brew upgrade python # this installs now Python3.7 which breaks libs
 # following https://stackoverflow.com/questions/51125013/how-can-i-install-a-previous-version-of-python-3-in-macos-using-homebrew/51125014#51125014
 # to install Python 3.6.5
-brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+brew upgrade https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
 brew install p7zip
 #curl -L -O https://download.qt.io/archive/qt/5.10/5.10.0/single/qt-everywhere-src-5.10.0.tar.xz
 #tar xzf qt-everywhere-src-5.10.0.tar.xz
