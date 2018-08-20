b97592dfc3709d07fbb847b9cded9bc1c88514c0
Rui Paulo <rpaulo@me.com>
2018-08-19 22:59:29 -0700

    Move OSX build to python 3.6

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 set -ex
-brew upgrade python
+
+#brew upgrade python
 
 # following https://stackoverflow.com/questions/51125013/how-can-i-install-a-previous-version-of-python-3-in-macos-using-homebrew/51125014#51125014
 # to install Python 3.6.5
-#brew remove --ignore-dependencies python
-#brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+brew remove --ignore-dependencies python
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
 
 brew install p7zip
 
 pip3 install -r src/requirements.txt
 pip3 install -r src/requirements-${TRAVIS_OS_NAME}.txt
-sudo rm -rf /usr/local/lib/python3.7/site-packages/matplotlib/mpl-data/sample_data
+sudo rm -rf /usr/local/lib/python3.6/site-packages/matplotlib/mpl-data/sample_data
 
 .travis/install-phidgets.sh
 .travis/install-snap7.sh
--- src/build-mac3.sh
+++ src/build-mac3.sh
@@ -5,15 +5,15 @@ set -ex
 
 if [ ! -z $TRAVIS ]; then
     export PYTHON=/usr/local
-    export PYTHONPATH=$PYTHON/lib/python3.7
-    export PYTHON_V=3.7
+    export PYTHONPATH=$PYTHON/lib/python3.6
+    export PYTHON_V=3.6
     export QT_PATH=${PYTHONPATH}/site-packages/PyQt5/Qt
     export QT_SRC_PATH=${QT_PATH}
     export MACOSX_DEPLOYMENT_TARGET=10.13
 else
-    export PYTHON=/Library/Frameworks/Python.framework/Versions/3.7
-    export PYTHONPATH=$PYTHON/lib/python3.7
-    export PYTHON_V=3.7
+    export PYTHON=/Library/Frameworks/Python.framework/Versions/3.6
+    export PYTHONPATH=$PYTHON/lib/python3.6
+    export PYTHON_V=3.6
     export QT_PATH=${PYTHONPATH}/site-packages/PyQt5/Qt
     export QT_SRC_PATH=~/Qt5.11.1/5.11.1/clang_64
     export MACOSX_DEPLOYMENT_TARGET=10.13
