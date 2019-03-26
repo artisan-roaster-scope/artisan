27bd65243db544871b003a563c103f299d0a6b18
MAKOMO <marko.luther@gmx.net>
2019-03-26 22:16:59 +0100

    another try to get keyring working on Linux builds

--- src/plus/connection.py
+++ src/plus/connection.py
@@ -127,9 +127,9 @@ def setKeyring():
                     # if priority is not 0, we set it as keyring system
                     keyring.set_keyring(ss_keyring)
             except Exception as e:
-                import sys
-                _, _, exc_tb = sys.exc_info()
-                config.logger.error("controller: Linux keyring Exception %s",e, exc_tb.tb_lineno)
+#                import sys
+#                _, _, exc_tb = sys.exc_info()
+#                config.logger.error("controller: Linux keyring Exception %s",e, exc_tb.tb_lineno)
                 pass               
         #config.logger.debug("keyring: %s",str(keyring.get_keyring()))
     except Exception as e:
