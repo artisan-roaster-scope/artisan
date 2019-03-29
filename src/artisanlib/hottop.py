147627ff45febf2088909e971acf244414520a31
MAKOMO <marko.luther@gmx.net>
2019-03-29 10:38:27 +0100

    updates doc

--- src/artisanlib/hottop.py
+++ src/artisanlib/hottop.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ABOUT
+# Hottop 2k+ support for Artisan
+
+# LICENSE
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 2 of the License, or
+# version 3 of the License, or (at your option) any later versison. It is
+# provided for educational purposes and is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+# the GNU General Public License for more details.
+
+# AUTHOR
+# Marko Luther, 2019
+
 from multiprocessing import Process, Lock
 from multiprocessing.sharedctypes import Value
 from ctypes import c_bool, c_double
--- src/artisanlib/s7port.py
+++ src/artisanlib/s7port.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ABOUT
+# S7 support for Artisan
+
+# LICENSE
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 2 of the License, or
+# version 3 of the License, or (at your option) any later versison. It is
+# provided for educational purposes and is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+# the GNU General Public License for more details.
+
+# AUTHOR
+# Marko Luther, 2018
+
 import time
 import platform
 import struct
