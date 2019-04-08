329d7cb579e60eeaf8dab8f0795e532fc79969e2
Rui Paulo <rpaulo@me.com>
2019-04-07 18:10:24 -0700

    Qemu: replace the -redir option.

--- src/build-rpi2-qemu.sh
+++ src/build-rpi2-qemu.sh
@@ -157,4 +157,4 @@ sudo losetup -d /dev/loop0
 rmdir $mountpoint
 
 ssh_control &
-qemu-system-arm -kernel ${KERNEL_IMAGE} -dtb versatile-pb.dtb -cpu arm1176 -m 256 -M versatilepb -no-reboot -nographic -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -hda ${RASPBIAN_IMAGE} -redir tcp:2222::22
+qemu-system-arm -kernel ${KERNEL_IMAGE} -dtb versatile-pb.dtb -cpu arm1176 -m 256 -M versatilepb -no-reboot -nographic -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -hda ${RASPBIAN_IMAGE} -netdev user,id=ethernet.0,hostfwd=tcp:2222::22
