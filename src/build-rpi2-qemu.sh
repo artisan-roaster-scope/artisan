3856e260d9a58bec32f2bd17a0ad57b72679ff2d
Rui Paulo <rpaulo@me.com>
2019-04-27 10:44:02 -0700

    Debugging more qemu problems.

--- src/build-rpi2-qemu.sh
+++ src/build-rpi2-qemu.sh
@@ -157,6 +157,7 @@ sudo losetup -d /dev/loop0
 rmdir $mountpoint
 
 #ssh_control &
-ulimit -c unlimited
+qemu-system-arm || true
+qemu-system-arm -help || true
 qemu-system-arm -kernel ${KERNEL_IMAGE} -dtb versatile-pb.dtb -cpu arm1176 -m 256 -M versatilepb -no-reboot -nographic -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -drive format=raw,file=${RASPBIAN_IMAGE}
 #-netdev user,id=ethernet.0,hostfwd=tcp::2222-:22
