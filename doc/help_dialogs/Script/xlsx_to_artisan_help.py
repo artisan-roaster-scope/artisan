cae05a13715f753d66bbe257c8156830d46f1067
Dave <dave@roasterdave.com>
2020-03-31 19:53:08 -0700

    updates all help dialogs to new format, content unchanged

--- doc/help_dialogs/Script/xlsx_to_artisan_help.py
+++ doc/help_dialogs/Script/xlsx_to_artisan_help.py
@@ -5,7 +5,7 @@ Command line:  xlsx_to_artisan_help.py [<filename>]
 **Note that filename should have no suffix.  File name only.
 
 Input file is always drawn from ../input_files/<filename>.xlsx
-If filename is not specified 'help_pages' will be used.
+If filename is not specified 'test' will be used.
 
 Output is always written to ../output_files/<filename>.py
     which will be executed and writes to ../output_files/<filename>.html
@@ -25,9 +25,10 @@ Excel file format:
     Table data rows                  [Multiple coniguous rows, optional but must follow the table headers]
     Bottom Note                      [Multiple contiguous rows, optional]
 
-    Cells formatted as Italic or with any font color will not be sent to QApplication.translate().
+    QApplication.translate() is not applied to cells formatted as Italic, formatted with any font color 
+    or if the cell has a numeric value.
     
-    Use only double quotes in cell values, no single quotes.
+    Beware: Single quotes in cells will be replaced with double quotes.
     
     Single blank rows are legal. Consecutive blank rows are interpreted as the end of a table. All cells in 
     a blank row must be empty.
@@ -62,6 +63,9 @@ import prettytable
 
 def u(x):
     return str(x)
+    
+ind = "        "     #indent
+nlind = "\n" + ind   #new line plus indent
 
 def translateStr(str, group='HelpDlg'):
     t_str = "u(QApplication.translate('" + group + "','" + u(str) + "',None))"
@@ -72,30 +76,33 @@ def generateRows(ws):
     for row in ws.iter_rows():
         this_row = []
         for cell in row:
-            if cell.data_type == 's': 
-                if cell.font.italic or \
-                        (cell.font.color != None and \
-                         type(cell.font.color.rgb) == str and \
-                         cell.font.color.rgb != "FF000000"):    
-                    cell_str = "'" + cell.value + "'"           
-                else:
-                    cell_str = translateStr(cell.value)
-            else:
-                cell_str = str(cell.value)
-            
             # insert a nonbreaking space into blank cells to keep the row height homgenous
-            if cell.value == None or len(cell_str) == 0:
+            if cell.value == None or cell.value == '':
                 cell_str = "'&#160;'"
-
+            else:
+                if cell.data_type == 's': 
+                    cell_value = re.subn(r"'",r'"',cell.value)  #protect downstream by changing single quotes to double 
+                    # do not translate if the cell has italic format or any font color
+                    if cell.font.italic or \
+                            (cell.font.color != None and \
+                             type(cell.font.color.rgb) == str and \
+                             cell.font.color.rgb != "FF000000"):    
+                        cell_str = "'" + cell_value[0] + "'"           
+                    else:
+                        cell_str = translateStr(cell_value[0])
+                else:
+                    # numeric value
+                    cell_str = str(cell.value)
+            
             this_row.append(cell_str)
         all_rows.append(this_row)
     return all_rows
 
 def getTitle(all_rows,ws,nsheet):
     if nsheet == 0:
-        title = '\n' + 'helpstr += "<b>" + ' + str(all_rows[0][0]) + ' + "</b>"'
+        title = nlind + 'helpstr += "<b>" + ' + str(all_rows[0][0]) + ' + "</b>"'
     else:
-        title = '\n' + 'helpstr += "<br/><br/><b>" + ' + str(all_rows[0][0]) + ' + "</b>"'
+        title = nlind + 'helpstr += "<br/><br/><b>" + ' + str(all_rows[0][0]) + ' + "</b>"'
     return title
   
 def getNotes(all_rows,nrows,tbl_name,notetype='top'):
@@ -112,9 +119,9 @@ def getNotes(all_rows,nrows,tbl_name,notetype='top'):
             notes.append(gotnote[0])
             notes_len += 1
     notes = '+newline+'.join(notes)
-    tbl_notes += '\n' + tbl_name + ' = prettytable.PrettyTable()'
-    tbl_notes += '\n' + tbl_name + '.header = False'
-    tbl_notes += '\n' + tbl_name + '.add_row([' + notes + '])'
+    tbl_notes += nlind + tbl_name + ' = prettytable.PrettyTable()'
+    tbl_notes += nlind + tbl_name + '.header = False'
+    tbl_notes += nlind + tbl_name + '.add_row([' + notes + '])'
 
     return tbl_notes, notes_len
 
@@ -129,7 +136,7 @@ def getAddrows(all_rows,tbl_name):
     for idx in range(len(all_rows)):
         this_row = ','.join(all_rows[idx])
         if idx > 0:
-            addrows += "\n"
+            addrows += nlind
         addrows += u(tbl_name) + ".add_row([" + this_row + "])"
     return addrows
 
@@ -146,19 +153,21 @@ def buildpyCode(fname_in,fname_out_html):
     outstr += '\n' + 'from PyQt5.QtWidgets import QApplication'
     outstr += '\n' + 'def u(x):'
     outstr += '\n' + '    return str(x)'
+    outstr += '\n' + 'if True:  #here just to achieve the indent'
+    outstr += '\n' + '    if True:'
     outstr += '\n# ----- Cut below ---------------------------------------------'
     
     # initialize the output string
     outstr += '\n'
-    outstr += '\n' + '# autogenerated help pasted below'
+    outstr += nlind + '# autogenerated help pasted below'
     outstr += '\n'
-    outstr += '\n' + 'newline = "\\n"  #@UnusedVariable'
-    outstr += '\n' + 'helpstr = ""'
-    outstr += '\n' + 'helpstr += "<head><style>"'
-    outstr += '\n' + 'helpstr += "td, th {border: 1px solid #ddd;  padding: 6px;}"'
-    outstr += '\n' + 'helpstr += "th {padding-top: 6px;padding-bottom: 6px;text-align: left;background-color: #0C6AA6; color: white;}"'
-    outstr += '\n' + 'helpstr += "</style></head>"'
-    outstr += '\n' + 'helpstr += "<body>"'
+    outstr += nlind + 'newline = "\\n"  #@UnusedVariable'
+    outstr += nlind + 'helpstr = ""'
+    outstr += nlind + 'helpstr += "<head><style>"'
+    outstr += nlind + 'helpstr += "td, th {border: 1px solid #ddd;  padding: 6px;}"'
+    outstr += nlind + 'helpstr += "th {padding-top: 6px;padding-bottom: 6px;text-align: left;background-color: #0C6AA6; color: white;}"'
+    outstr += nlind + 'helpstr += "</style></head>"'
+    outstr += nlind + 'helpstr += "<body>"'
 
     # Open the workbook
     wb = load_workbook(filename = fname_in, read_only=False)
@@ -212,29 +221,29 @@ def buildpyCode(fname_in,fname_out_html):
         # add  top notes
         if nrows_topnotes > 0:
             outstr += tbl_topnotes
-            outstr += '\n' + 'helpstr += ' + tbl_name + 'top' + '.get_html_string(attributes={' + note_table_attributes + '})'
+            outstr += nlind + 'helpstr += ' + tbl_name + 'top' + '.get_html_string(attributes={' + note_table_attributes + '})'
 
         # add the data table with header
         if len(tbl_addrows) > 0:
-            outstr += '\n' + tbl_name + ' = prettytable.PrettyTable()'
-            outstr += '\n' + tbl_fieldnames
-            outstr += '\n' + tbl_addrows
-            outstr += '\n' + 'helpstr += ' + tbl_name + '.get_html_string(attributes={' + data_table_attributes + '})'
+            outstr += nlind + tbl_name + ' = prettytable.PrettyTable()'
+            outstr += nlind + tbl_fieldnames
+            outstr += nlind + tbl_addrows
+            outstr += nlind + 'helpstr += ' + tbl_name + '.get_html_string(attributes={' + data_table_attributes + '})'
 
         # add bottom notes
         if nrows_bottomnotes > 0:
             outstr += tbl_bottomnotes
-            outstr += '\n' + 'helpstr += ' + tbl_name + 'bottom' + '.get_html_string(attributes={' + note_table_attributes + '})'
+            outstr += nlind + 'helpstr += ' + tbl_name + 'bottom' + '.get_html_string(attributes={' + note_table_attributes + '})'
         
     # finalize outstr - py code
-    outstr += '\n' + 'helpstr += "</body>"'
+    outstr += nlind + 'helpstr += "</body>"'
     
     # add ugly stuff to clean the nbsp entity that gets escaped in the html output by PrettyTable
-    outstr += '\n' + 'helpstr = re.sub(r"&amp;#160;", r"&#160;",helpstr)'
+    outstr += nlind + 'helpstr = re.sub(r"&amp;#160;", r"&#160;",helpstr)'
 
     # add trailer
     outstr += '\n'
-    outstr += '\n' + '# autogenerated help pasted above'
+    outstr += nlind + '# autogenerated help pasted above'
     outstr += '\n'
     outstr += '\n# ----- Cut above ---------------------------------------------'
     # write to named html file
@@ -259,9 +268,9 @@ if __name__ == "__main__":
         fname_out = '../output_files/' + sys.argv[1] + '.py'
         fname_out_html = '../output_files/' + sys.argv[1] + '.html'
     else:  #for testing
-        fname_in = '../input_files/help_pages.xlsx'
-        fname_out = '../output_files/help_pages.py'
-        fname_out_html = '../output_files/help_pages.html'
+        fname_in = '../input_files/test.xlsx'
+        fname_out = '../output_files/test.py'
+        fname_out_html = '../output_files/test.html'
 
     outstr = buildpyCode(fname_in,fname_out_html)
 #    print(outstr)
