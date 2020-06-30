d97a1d7ff6c75a2fe2e939bbadd4c08c7469a6e0
MAKOMO <marko.luther@gmx.net>
2020-06-30 09:32:30 +0200

    upgrade pip on linux

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -8,6 +8,7 @@ sudo apt-get install ruby-dev build-essential p7zip-full rpm gdb libudev-dev qt5
 sudo apt-get install -y libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0
 
 gem install fpm
+pip3 install --upgrade pip
 pip3 install -r src/requirements.txt
 pip3 install -r src/requirements-${TRAVIS_OS_NAME}.txt
 
