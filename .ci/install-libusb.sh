09e215cfcb5384daedc02ff0716ab792ea38568b
Dave <dave@roasterdave.com>
2021-01-14 08:02:18 -0800

    with deploy

--- .ci/install-libusb.sh
+++ .ci/install-libusb.sh
@@ -7,7 +7,7 @@ if [ "$ARTISAN_OS" != "linux" ]; then
    exit 0
 fi
 
-sudo apt-get remove libusb-1.0-0
+sudo apt-get remove -y libusb-1.0-0
 # we use the -k option in the following as the SSL certificate on sourceforge does not validate
 
 ##curl -k -L -O https://kent.dl.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.21/libusb-1.0.21.tar.bz2
