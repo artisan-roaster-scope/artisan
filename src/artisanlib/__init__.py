f5e514e1e38ae430ac3e8b1ae73b8f29c5c9dd07
MAKOMO <marko.luther@gmx.net>
2021-06-19 16:59:55 +0200

    correct release sponsor name

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -4,6 +4,6 @@ __version__ = '2.4.5'
 __revision__ = '0'
 __build__ = '0'
 
-__release_sponsor_name__ = "Sweet Maria"
+__release_sponsor_name__ = "Sweet Maria's"
 __release_sponsor_domain__ = "sweetmarias.com"
 __release_sponsor_url__ = "https://www.sweetmarias.com/"
