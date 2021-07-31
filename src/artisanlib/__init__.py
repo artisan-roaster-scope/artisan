0027b92ccad1f4d61eb49cf8cf755c9eaded51cb
MAKOMO <marko.luther@gmx.net>
2021-07-31 08:56:39 +0200

    push version

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '2.4.6'
+__version__ = '2.4.7'
 __revision__ = '0'
-__build__ = '1'
+__build__ = '0'
 
 __release_sponsor_name__ = "Sweet Maria's"
 __release_sponsor_domain__ = "sweetmarias.com"
