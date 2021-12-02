a0a480815bcff74e129769e46168650ccc428632
MAKOMO <marko.luther@gmx.net>
2021-12-02 23:04:08 +0100

    fix macOS build script

--- src/build-mac3.sh
+++ src/build-mac3.sh
@@ -8,17 +8,27 @@ set -e  # reduced logging
 if [ ! -z $APPVEYOR ]; then
     # Appveyor CI builds
     echo "NOTICE: Appveyor build"
+    export PYTHON_V=3.9
     export PYTHON=/Users/appveyor/venv3.9.6
     export PYTHONBIN=$PYTHON/bin
     export PYTHONPATH=$PYTHON/lib/python3.9
-    export PYTHON_V=3.9
-    export PYLUPDATE=$PYTHONBIN/pylupdate5
+
+# for PyQt5:
+#    export PYLUPDATE=$PYTHONBIN/pylupdate5
+#    export QT_PATH=${PYTHONPATH}/site-packages/PyQt5/Qt5 # from PyQt v5.15.4 this dir changed form PyQt5/Qt to PyQt5/Qt5
+#    export QT_SRC_PATH=${QT_PATH}
+#    export PYUIC=pyuic5
+#    export PYRCC=pyrcc5
     
-    export QT_PATH=${PYTHONPATH}/site-packages/PyQt5/Qt5 # from PyQt v5.15.4 this dir changed form PyQt5/Qt to PyQt5/Qt5
-    export QT_SRC_PATH=${QT_PATH}
-    export PYUIC=pyuic5
-    export PYRCC=pyrcc5
+# for PyQt6
+    export QT_PATH=${PYTHONPATH}/site-packages/PyQt6/Qt6
+    export QT_SRC_PATH=~/Qt6/6.2.0/macos
+    export PYUIC=pyuic6
+    export PYRCC=pyrcc6
+    export PYLUPDATE=./pylupdate6pro
+    
     export MACOSX_DEPLOYMENT_TARGET=10.15
+    export DYLD_LIBRARY_PATH=$PYTHON/lib:$DYLD_LIBRARY_PATH
 else
     # standard local builds
     echo "NOTICE: Standard build"
