56060883de096c7ece20f05b6f45eef3da6fa65d
MAKOMO <marko.luther@gmx.net>
2021-12-14 09:26:38 +0100

    push lxml to 4.7.1

--- .ci/install-osx.sh
+++ .ci/install-osx.sh
@@ -41,7 +41,7 @@ python3 -m pip install --upgrade pip
 # (an alternative could be to use pip install --user ..)
 # the lxml binaries are compiled with an SDK older than the 10.9 SDK which breaks the notarization
 # thus we force the compilation from source
-sudo -H python3 -m pip install --no-binary lxml lxml==4.6.5 # 4.6.4
+sudo -H python3 -m pip install --no-binary lxml lxml==4.7.1 #4.6.5 # 4.6.4
 sudo -H python3 -m pip install -r src/requirements.txt
 # replaced sudo -H python3 -m pip install -r src/requirements-${TRAVIS_OS_NAME}.txt
 sudo -H python3 -m pip install -r src/requirements-${ARTISAN_OS}.txt
