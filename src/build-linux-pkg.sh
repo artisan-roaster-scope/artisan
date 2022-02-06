ad32ad64292897f795812d627c8b369cbd2e92eb
MAKOMO <marko.luther@gmx.net>
2022-02-06 23:43:47 +0100

    indicate architecture

--- src/build-linux-pkg.sh
+++ src/build-linux-pkg.sh
@@ -75,10 +75,11 @@ cd ..
 mv *.rpm ${NAME}.rpm
 mv *.deb ${NAME}.deb
 
+export ARCH=x86_64
 # Create AppImage by using the pkg2appimage tool
 wget -c https://github.com/$(wget -q https://github.com/AppImage/pkg2appimage/releases -O - | grep "pkg2appimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
 chmod +x ./pkg2appimage-*.AppImage
-./pkg2appimage-*.AppImage artisan-AppImage.yml
+ARCH=x86_64 ./pkg2appimage-*.AppImage artisan-AppImage.yml
 
 mv ./out/*.AppImage ${NAME}.AppImage
 
