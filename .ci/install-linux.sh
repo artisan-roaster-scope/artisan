c6e2556bda7f84634be2c49fb7be59abef41b5cf
MAKOMO <marko.luther@gmx.net>
2023-05-05 12:15:19 +0200

    - fixes startup on Ubuntu Wayland (Issue #1001) - adds missing libxcb-cursor0 to startup on Ubuntu XOrg with Qt6.5

--- .ci/install-linux.sh
+++ .ci/install-linux.sh
@@ -5,8 +5,8 @@ sudo apt-get update -y -q
 sudo apt-get install -y -q ruby-dev build-essential p7zip-full rpm gdb libudev-dev qt5-default
 sudo apt-get install -y -q fakeroot
 
-# add libs not installed by default on Qt5.15 any longer
-sudo apt-get install -y -q libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0
+# add libs not installed by default on Qt5.15/Qt6 any longer
+sudo apt-get install -y -q libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 libxcb-cursor0
 
 gem install fpm -v 1.12.0 # Linux build fails using 1.13.0
 pip install --upgrade pip
