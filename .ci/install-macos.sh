22ba2d5aecef145b0b264246ab233926bc52b096
MAKOMO <marko.luther@gmx.net>
2023-08-19 23:06:22 +0200

    another try

--- .ci/install-macos.sh
+++ .ci/install-macos.sh
@@ -30,12 +30,14 @@ if [ -n "${PYUPGRADE_V:-}" ]; then
     brew update && brew upgrade python
     # relink Python
     brew unlink python@${PYTHON_V} && brew link --force --overwrite python@${PYTHON_V}
+    hash -r
     # add path
     export PATH="$(brew --prefix)/Cellar/python@${PYTHON_V}/${PYUPGRADE_V}/bin:${PATH}"
 
-    ls -l "$(brew --prefix)/Cellar/python@${PYTHON_V}/${PYUPGRADE_V}"
-    ls -l "$(brew --prefix)/Cellar/python@${PYTHON_V}/${PYUPGRADE_V}/bin"
-    ls -l "$(brew --prefix)/Cellar/python@${PYTHON_V}/${PYUPGRADE_V}/Frameworks"
+    ls -l "$(brew --prefix)/Cellar/python@${PYTHON_V}/
+#    ls -l "$(brew --prefix)/Cellar/python@${PYTHON_V}/${PYUPGRADE_V}"
+#    ls -l "$(brew --prefix)/Cellar/python@${PYTHON_V}/${PYUPGRADE_V}/bin"
+#    ls -l "$(brew --prefix)/Cellar/python@${PYTHON_V}/${PYUPGRADE_V}/Frameworks"
     echo $PATH
     which python3
     python3 --version
