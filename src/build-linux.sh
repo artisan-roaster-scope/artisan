78d320df0aa7c16190dd9089d7dce42ca0af5c92
MAKOMO <marko.luther@gmx.net>
2023-11-28 12:07:39 +0100

    fix the fix

--- src/build-linux.sh
+++ src/build-linux.sh
@@ -103,8 +103,8 @@ cp includes/SourceHanSansJP-Regular.otf dist
 cp includes/SourceHanSansKR-Regular.otf dist
 cp includes/SourceHanSansTW-Regular.otf dist
 cp includes/dijkstra.ttf dist
-cp includes/ComicNeue-Regular.ttf
-cp includes/xkcd-script.ttf
+cp includes/ComicNeue-Regular.ttf dist
+cp includes/xkcd-script.ttf dist
 cp includes/alarmclock.eot dist
 cp includes/alarmclock.svg dist
 cp includes/alarmclock.ttf dist
