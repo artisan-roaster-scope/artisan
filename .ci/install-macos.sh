0f2fb733765ce491e947b55c818cf9c8ba36d589
Dave <dave@roasterdave.com>
2024-02-14 12:09:51 -0800

    -allow Python upgrade on Windows
    -pin dotenv version on Linux

--- .ci/install-macos.sh
+++ .ci/install-macos.sh
@@ -21,8 +21,8 @@ set -e # reduced logging
 echo "** Running install-macos.sh"
 #.ci/silence.sh brew update # this seems to help to work around some homebrew issues; and fails on others
 
-# upgrade Python version when PYUPGRADE_V exists and has a value
-if [ -n "${PYUPGRADE_V:-}" ]; then
+# upgrade Python version when PYUPGRADE_MACOS_V exists and has a value
+if [ -n "${PYUPGRADE_MACOS_V:-}" ]; then
     # first deactivate current venv
     source ${VIRTUAL_ENV}/bin/activate
     deactivate
@@ -41,11 +41,11 @@ if [ -n "${PYUPGRADE_V:-}" ]; then
     brew reinstall openssl # needed to get the ssl certificates properly installed for artisan.plus communication
 
     # create new venv
-    python3 -m venv /Users/appveyor/venv${PYUPGRADE_V}
-    source /Users/appveyor/venv${PYUPGRADE_V}/bin/activate
+    python3 -m venv /Users/appveyor/venv${PYUPGRADE_MACOS_V}
+    source /Users/appveyor/venv${PYUPGRADE_MACOS_V}/bin/activate
     # update symbolic link to point to our new venv
-    ln -vfns /Users/appveyor/venv${PYUPGRADE_V} /Users/appveyor/venv${PYTHON_V}
-    export PATH=/Users/appveyor/venv${PYUPGRADE_V}/bin:${PATH} # not exported?
+    ln -vfns /Users/appveyor/venv${PYUPGRADE_MACOS_V} /Users/appveyor/venv${PYTHON_V}
+    export PATH=/Users/appveyor/venv${PYUPGRADE_MACOS_V}/bin:${PATH} # not exported?
 fi
 
 hash -r
