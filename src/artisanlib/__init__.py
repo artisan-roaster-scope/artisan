336b525762734d7d6ffce87e4e380b4af809268b
MAKOMO <MAKOMO@users.noreply.github.com>
2024-07-31 21:26:08 +0200

    ping

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -2,7 +2,6 @@ __version__ = '3.0.0'
 __revision__ = ''
 __build__ = '0'
 
-
 __release_sponsor_name__ = 'artisan.plus'
 __release_sponsor_domain__ = 'artisan.plus'
 __release_sponsor_url__ = 'https://artisan.plus/'
