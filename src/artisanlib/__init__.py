88ccf63b1f44d4cbc66810107e3562afbc93e994
MAKOMO <MAKOMO@users.noreply.github.com>
2026-02-16 17:41:34 +0100

    adds SUSA machine setup

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '4.0.2'
+__version__ = '4.0.3'
 __revision__ = ''
 __build__ = '0'
 
--- src/artisanlib/kaleido.py
+++ src/artisanlib/kaleido.py
@@ -62,11 +62,11 @@ class KaleidoPort:
         self._default_data_stream:Final[str] = 'A0'
         self._open_timeout:Final[float] = 6      # in seconds
         self._init_timeout:Final[float] = 6      # in seconds
-        self._ping_timeout:Final[float] = 0.8    # in seconds
-        self._send_timeout:Final[float] = 0.4    # in seconds
-        self._read_timeout:Final[float] = 4      # in seconds
+        self._ping_timeout:Final[float] = 1.2    # in seconds
+        self._send_timeout:Final[float] = 0.6    # in seconds
+        self._read_timeout:Final[float] = 5      # in seconds
         self._ping_retry_delay:Final[float] = 1  # in seconds
-        self._reconnect_delay:Final[float] = 0.2 # in seconds
+        self._reconnect_delay:Final[float] = 0.5 # in seconds
 
         self.send_button_timeout:Final[float] = 1.2  # in seconds
 
