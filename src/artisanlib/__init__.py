02d73e693da6c6bbd061c953de8fdf059474f0f1
MAKOMO <MAKOMO@users.noreply.github.com>
2026-05-21 16:49:46 +0200

    test2

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '4.0.3'
-__revision__ = ''
+__revision__ = '649d5f9fd'
 __build__ = '0'
 __artisan_os__ = 'Linux'
-__signature__ = ''
 
 __release_sponsor_name__ = 'artisan.plus'
 __release_sponsor_domain__ = 'artisan.plus'
 __release_sponsor_url__ = 'https://artisan.plus/'
+__signature__ = 'f2021ba2f6a3b4b54d5167936d9dfef25f43720af6b5154c0fd63c8f8ced7acdfbdb297bf2b26ff28b1c337b719abf4f57987894d2fc9cc1a746f41178053005'
--- src/artisanlib/comm.py
+++ src/artisanlib/comm.py
@@ -10,7 +10,7 @@
 # provided for educational purposes and is distributed in the hope that
 # it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-# the GNU General Public License for more details..
+# the GNU General Public License for more details.
 
 # AUTHOR
 # Marko Luther, 2023
